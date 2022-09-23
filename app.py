3c003982b2d0fb370c2159018f7bac09c6ca4b99
HackerBurscht <noe.kraettli@gmx.ch>
2022-09-23 09:00:12 +0200

    Setup Database testing

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
-from flask import Flask
-
+from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///maindatabase.db"
+db = SQLAlchemy(app)
+
+class meal(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(100), nuullable=1)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+
+def __repr__(self):
+    return "<Task %r>" % self.id
 
 @app.route("/")
 
