34421c130b3c90cd9646e64adfc398a8a5ac0131
HackerBurscht <noe.kraettli@gmx.ch>
2022-09-23 15:32:43 +0200

    Database running, working on templates

--- app.py
+++ app.py
@@ -1,25 +1,26 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///mealy.db"
 db = SQLAlchemy(app)
 
 
 class Meal(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.String(100), nuullable=True)
+    content = db.Column(db.String(100), nullable=True)
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
         return "<Task %r>" % self.id
 
 
-@app.route("/")
+@app.route("/", methods=["POST", "GET"])
 def index():
     return render_template("index.html")
 
 
-if __name__ == "__name__":
+if __name__ == "__main__":
     app.run(debug=True)
