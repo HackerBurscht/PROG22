bdbe5b7abf3e3d30482c2aef94970d6cdc9ae530
HackerBurscht <noe.kraettli@gmx.ch>
2022-09-23 09:32:11 +0200

    Mama mia

--- app.py
+++ app.py
@@ -1,24 +1,25 @@
 from flask import Flask, render_template, url_for
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///maindatabase.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///mealy.db"
 db = SQLAlchemy(app)
 
-class meal(db.Model):
+
+class Meal(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.String(100), nuullable=1)
+    content = db.Column(db.String(100), nuullable=True)
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
+    def __repr__(self):
+        return "<Task %r>" % self.id
 
-def __repr__(self):
-    return "<Task %r>" % self.id
 
 @app.route("/")
-
 def index():
     return render_template("index.html")
 
+
 if __name__ == "__name__":
     app.run(debug=True)
-
