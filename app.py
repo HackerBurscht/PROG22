b99d7ad8348c5a25f496ebdeb989bc3e0f7a65a0
HackerBurscht <noe.kraettli@gmx.ch>
2022-09-23 22:28:32 +0200

    Database running, some templates done, post to database now working, rerouting figured out

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from flask import Flask, render_template, url_for, request
+from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path="/static")  # Fick mein Leben. 5 Stunden für ein IMG.
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///mealy.db"
 db = SQLAlchemy(app)
@@ -10,7 +10,8 @@ db = SQLAlchemy(app)
 
 class Meal(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.String(100), nullable=True)
+    content = db.Column(db.String(100), nullable=False)
+    weekday = db.Column(db.String(2), nullable=False)
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
@@ -19,7 +20,28 @@ class Meal(db.Model):
 
 @app.route("/", methods=["POST", "GET"])
 def index():
-    return render_template("index.html")
+    if request.method == "POST":
+        content = request.form.get("form_mo")
+        if content == "":
+            return redirect("/")
+        elif content != "":
+            new_content = Meal(content=content, weekday="Mo")
+
+        try:
+            db.session.add(new_content)
+            db.session.commit()
+            return redirect("/")
+        except:
+            return "Gab nen Fehler"
+
+    else:
+        all_content = Meal.query.order_by(Meal.date_created).all()
+        return render_template("index.html")
+
+
+@app.route("/About")
+def about():
+    return render_template("about.html")
 
 
 if __name__ == "__main__":
