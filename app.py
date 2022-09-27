8a721edd531aebc4c5881d66ab028d5b3bf4b08f
HackerBurscht <noe.kraettli@gmx.ch>
2022-09-27 17:05:50 +0200

    Switched from SQL-db to JSON-File and restyled the functions to save data.

--- app.py
+++ app.py
@@ -1,23 +1,9 @@
 from flask import Flask, render_template, url_for, request, redirect
-from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime, timedelta
+import json
 
 app = Flask(__name__, static_url_path="/static")  # Fick mein Leben. 5 Stunden für ein IMG.
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///mealy.db"
-db = SQLAlchemy(app)
 week_key = ""
-
-
-class Meal(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.String(100), nullable=False)
-    weekday = db.Column(db.String(2), nullable=False)
-    date_created = db.Column(db.DateTime, default=datetime.utcnow)
-    planned_date = db.Column(db.String(12), nullable=False)
-
-    def __repr__(self):
-        return "<Task %r>" % self.id
 
 
 @app.route("/", methods=["POST", "GET"])
@@ -28,33 +14,46 @@ def index():
     week_end = week_end.strftime("%d.%m.%Y")
     week_start = week_start.strftime("%d.%m. bis ")
 
-
-    item_mo = datetime.today() - timedelta(days=datetime.today().weekday() % 7)
-    item_mo = item_mo.strftime("%d.%m.%Y")
-    ph_mo = Meal.query.filter_by(planned_date=item_mo)
-    print(ph_mo)
-
+    read_jason()
 
     if request.method == "POST":
         pass
 
     else:
-        all_content = Meal.query.order_by(Meal.date_created).all()
-        return render_template("index.html", pass_week_start=week_start, pass_week_end=week_end, ph_mo=ph_mo)
+        return render_template("index.html", pass_week_start=week_start, pass_week_end=week_end)
 
 
-def savedb(week_key, planned_date_key):
+def save_info(week_key, planned_date_key):
     content = request.form.get(week_key)
     if content == "":
         return redirect("/")
-    elif content != "":
-        new_content = Meal(content=content, weekday=week_key, planned_date=planned_date_key)
+    else:
+        date = planned_date_key
+        temp_dic = {}
+        temp_dic[date] = {}
+        temp_dic[date]["content"] = content
+        temp_dic[date]["weekday"] = week_key
+        write_json(temp_dic)
 
-    try:
-        db.session.add(new_content)
-        db.session.commit()
-    except:
-        return "Gab nen Fehler"
+
+def write_json(temp_dic):
+    with open('content.json', 'r+') as f:
+        file_data = json.load(f)
+        file_data["content-file"].append(temp_dic)
+        f.seek(0)
+        json.dump(file_data, f, indent=4)
+    return redirect("/")
+
+
+def read_jason():
+    with open('content.json', "r+") as f:
+        d = json.load(f)
+        f.close()
+        item_mo = datetime.today() - timedelta(days=datetime.today().weekday() % 7)
+        item_mo = item_mo.strftime("%d.%m.%Y")
+        return_mo = str(d["content-file"][0][item_mo]["content"])
+        print(return_mo)
+        return render_template("index.html", pass_mo=return_mo)
 
 
 @app.route("/mo", methods=["POST", "GET"])
@@ -62,7 +61,7 @@ def save_mo():
     week_key = "mo"
     planned_date_key = datetime.today() - timedelta(days=datetime.today().weekday() % 7)
     planned_date_key = planned_date_key.strftime("%d.%m.%Y")
-    savedb(week_key, planned_date_key)
+    save_info(week_key, planned_date_key)
     return redirect("/")
 
 
@@ -71,7 +70,7 @@ def save_di():
     week_key = "di"
     planned_date_key = datetime.today() - timedelta(days=datetime.today().weekday() % 7) + timedelta(days=1)
     planned_date_key = planned_date_key.strftime("%d.%m.%Y")
-    savedb(week_key, planned_date_key)
+    save_info(week_key, planned_date_key)
     return redirect("/")
 
 
@@ -80,7 +79,7 @@ def save_mi():
     week_key = "mi"
     planned_date_key = datetime.today() - timedelta(days=datetime.today().weekday() % 7) + timedelta(days=2)
     planned_date_key = planned_date_key.strftime("%d.%m.%Y")
-    savedb(week_key, planned_date_key)
+    save_info(week_key, planned_date_key)
     return redirect("/")
 
 
@@ -89,7 +88,7 @@ def save_do():
     week_key = "do"
     planned_date_key = datetime.today() - timedelta(days=datetime.today().weekday() % 7) + timedelta(days=3)
     planned_date_key = planned_date_key.strftime("%d.%m.%Y")
-    savedb(week_key, planned_date_key)
+    save_info(week_key, planned_date_key)
     return redirect("/")
 
 
@@ -98,7 +97,7 @@ def save_fr():
     week_key = "fr"
     planned_date_key = datetime.today() - timedelta(days=datetime.today().weekday() % 7) + timedelta(days=4)
     planned_date_key = planned_date_key.strftime("%d.%m.%Y")
-    savedb(week_key, planned_date_key)
+    save_info(week_key, planned_date_key)
     return redirect("/")
 
 
@@ -107,7 +106,7 @@ def save_sa():
     week_key = "sa"
     planned_date_key = datetime.today() - timedelta(days=datetime.today().weekday() % 7) + timedelta(days=5)
     planned_date_key = planned_date_key.strftime("%d.%m.%Y")
-    savedb(week_key, planned_date_key)
+    save_info(week_key, planned_date_key)
     return redirect("/")
 
 
@@ -116,8 +115,9 @@ def save_so():
     week_key = "so"
     planned_date_key = datetime.today() - timedelta(days=datetime.today().weekday() % 7) + timedelta(days=6)
     planned_date_key = planned_date_key.strftime("%d.%m.%Y")
-    savedb(week_key, planned_date_key)
+    save_info(week_key, planned_date_key)
     return redirect("/")
+
 
 @app.route("/About")
 def about():
