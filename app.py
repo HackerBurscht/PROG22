f123b5107b83eac0fed9caecf099b3e0cdf407d8
HackerBurscht <noe.kraettli@gmx.ch>
2022-09-28 14:00:26 +0200

    Implemented a way to update json entries.

--- app.py
+++ app.py
@@ -8,33 +8,38 @@ week_key = ""
 
 @app.route("/", methods=["POST", "GET"])
 def index():
-    # Das Datum des ersten und des letzten Tages, der aktuellen Woche, wird berechnet und als String formatiert.
+    # Das Datum des ersten und des letzten Tages der aktuellen Woche wird berechnet und als String formatiert.
     week_start = datetime.today() - timedelta(days=datetime.today().weekday() % 7)
+    week_i1 = week_start
     week_end = week_start + timedelta(days=7)
+    week_i2 = week_end
     week_end = week_end.strftime("%d.%m.%Y")
     week_start = week_start.strftime("%d.%m. bis ")
-
-    i = week_start
-    while i <= week_end:
-        with open('content.json', "r+") as f:
-            d = json.load(f)
-            f.close()
-        x = str(d["content-file"][0][i]["content"])
-        print(x)
-        i += datetime.timedelta(days=1)
 
     with open('content.json', "r+") as f:
         d = json.load(f)
         f.close()
-        item_mo = datetime.today() - timedelta(days=datetime.today().weekday() % 7)
-        item_mo = item_mo.strftime("%d.%m.%Y")
-        return_mo = str(d["content-file"][0][item_mo]["content"])
+        i = week_i1
+        n = 0
+        x_list = []
+        while i < week_i2:
+            j = i.strftime("%d.%m.%Y")
+            x = str(d["content-file"][n][j]["content"])
+            x_list.append(x)
+            i += timedelta(days=1)
+            j = i.strftime("%d.%m.%Y")
+            n += 1
+        return_mo = x_list[0]
+        return_di = x_list[1]
+        return_mi = x_list[2]
+        return_do = x_list[3]
+        return_fr = x_list[4]
+        return_sa = x_list[5]
+        return_so = x_list[6]
 
-    if request.method == "POST":
-        pass
-
-    else:
-        return render_template("index.html", pass_week_start=week_start, pass_week_end=week_end, pass_mo=return_mo)
+        return render_template("index.html", week_start=week_start, week_end=week_end, return_mo=return_mo,
+                               return_di=return_di, return_mi=return_mi, return_do=return_do, return_fr=return_fr,
+                               return_sa=return_sa, return_so=return_so)
 
 
 def save_info(week_key, planned_date_key):
@@ -47,16 +52,18 @@ def save_info(week_key, planned_date_key):
         temp_dic[date] = {}
         temp_dic[date]["content"] = content
         temp_dic[date]["weekday"] = week_key
-        write_json(temp_dic)
-
-
-def write_json(temp_dic):
-    with open('content.json', 'r+') as f:
-        file_data = json.load(f)
-        file_data["content-file"].append(temp_dic)
-        f.seek(0)
-        json.dump(file_data, f, indent=4)
-    return redirect("/")
+        with open('content.json', 'r+') as f:
+            file_data = json.load(f)
+            if date in [file_data][0]["content-file"][0]:
+                print(date)
+                [file_data][0]["content-file"][0].update(temp_dic)
+                f.seek(0)
+                json.dump(file_data, f, indent=4)
+            else:
+                file_data["content-file"].append(temp_dic)
+                f.seek(0)
+                json.dump(file_data, f, indent=4)
+        return redirect("/")
 
 
 @app.route("/mo", methods=["POST", "GET"])
@@ -130,6 +137,11 @@ def about():
 @app.route("/contact")
 def contact():
     return render_template("contact.html")
+
+
+@app.route("/stats")
+def stats():
+    return render_template("stats.html")
 
 
 if __name__ == "__main__":
